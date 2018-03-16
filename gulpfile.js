31663b9add0073647d8665ec01a952a72d383720
Tolala9 <ly.an.mode1@gmail.com>
2018-03-16 12:53:40 +0200

    Made basic layout for blog page

--- gulpfile.js
+++ gulpfile.js
@@ -27,8 +27,9 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/fontawesome/fontawesome-all.min.js',
 		'app/js/common.min.js', // Always at the end
-		])
+		])  
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Minimyze oll js (on yout choice)
 	.pipe(gulp.dest('app/js'))
