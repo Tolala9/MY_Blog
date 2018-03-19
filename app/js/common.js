f45a1b05daf3d9b151429f229022674411ac6677
Tolala9 <ly.an.mode1@gmail.com>
2018-03-19 17:54:02 +0200

    Fix menu line for xs devices, Add toogle buton menu for xs devices

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,26 @@
 $(function() {
 
-	// Custom JS
+/*TOGGLE MENU*/
+
+	$(".toggle-mnu, .menu_item").click(function() {
+  $(".sandwich").toggleClass("active");
+});
+
+$(".toggle-mnu").click(function(){
+	if ($(".top-mnu").is(":visible")) {
+		$(".top-mnu").fadeOut(600);
+		$(".top-mnu li a").removeClass("fadeInUp animated");
+	} else {
+		$(".top-mnu").fadeIn(600);
+		$(".top-mnu li a").addClass("fadeInUp animated");
+	};
+}); 
+
+$(".top-mnu li a").click(function() {
+	$(".top-mnu").fadeOut(600);
+	$(".sandwich").toggleClass("active");
+}).append("<span>");
+/* END TOGGLE MENU */
+
 
 });
