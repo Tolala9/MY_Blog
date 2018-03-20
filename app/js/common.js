936e40b933b4e31497372e2c78eb6f83e5ca71cb
Tolala9 <ly.an.mode1@gmail.com>
2018-03-20 14:19:55 +0200

    Up Button added

--- app/js/common.js
+++ app/js/common.js
@@ -22,5 +22,16 @@ $(".top-mnu li a").click(function() {
 }).append("<span>");
 /* END TOGGLE MENU */
 
+$(window).scroll(function() {
+		if ($(this).scrollTop() > $(this).height()) {
+			$('.top').addClass('active');
+		} else {
+			$('.top').removeClass('active');
+		}
+	});
+	$('.top').click(function() {
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
+
 
 });
